65628a9bb72298d1db794096fc9922f67cee735d
Slava Koyfman <slava@koyfman.org>
2015-10-05 00:14:49 +0300

    Add parse-tree visitor to start generating NOTAM object (WIP)

--- notam.py
+++ notam.py
@@ -1,4 +1,5 @@
 import parsimonious
+import datetime
 
 grammar = parsimonious.Grammar(r"""
     root = "(" header __ q_clause __ a_clause __ b_clause __ c_clause __ (d_clause __)? e_clause (__ f_clause __ g_clause)? ")"
@@ -28,13 +29,9 @@ grammar = parsimonious.Grammar(r"""
     permanent = "PERM"
 
     d_clause = "D)" _ till_next_clause
-
     e_clause = "E)" _ till_next_clause
-
-    f_clause = "F)" _ (gnd / till_next_clause)
-    g_clause = "G)" _ (unl / till_next_clause)
-    gnd = "GND" / "SFC"
-    unl = "UNL"
+    f_clause = "F)" _ till_next_clause
+    g_clause = "G)" _ till_next_clause
 
     _ = " "
     __ = " " / "\n"
@@ -44,3 +41,115 @@ grammar = parsimonious.Grammar(r"""
     int3 = ~r"[0-9]{3}"
     till_next_clause = ~r".*?(?=(?:\)$)|(?:\s[A-Z]\)))"s
 """)
+
+class EstimatedDateTime(datetime.datetime):
+    """Represents an estimated point in time. Instances are identical to datetime.datetime in every way,
+    except that the instance will have an attribute 'is_estimated' which is set to True."""
+
+    def __new__(cls, *args, **kwargs):
+        """May be initialized either identically to a regular datetime.datetime or, optionally, by
+        providing an existing datetime object to copy."""
+
+        if not kwargs and len(args)==1 and isinstance(args[0], datetime.datetime):
+            other = args[0]
+            v = super().__new__(cls, other.year, other.month, other.day,
+                                     other.hour, other.minute, other.second,
+                                     other.microsecond, other.tzinfo)
+        else:
+            v = super().__new__(cls, *args, **kwargs)
+        v.is_estimated = True
+        return v
+
+class NotamParseVisitor(parsimonious.NodeVisitor):
+    def has_descendant(self, node, desc_name):
+        if node.expr_name == desc_name: return True
+        else: return any([self.has_descendant(c,desc_name) for c in node.children])
+
+    def visit_simple_regex(self, node, _): return node.match.group(0)
+
+    def visit_code_node(self, *args, meanings):
+        """Maps coded strings, where each character encodes a special meaning, into a corresponding decoded set
+        according to the meanings dictionary (see examples of usage further below)"""
+        codes = self.visit_simple_regex(*args)
+        return set([meanings[code] for code in codes])
+
+    def visit_notamX_header(self, node, visited_children, notam_type):
+        """Common code shared by NOTAM[N|C|R] headers"""
+        self.notam_id = visited_children[0]
+        self.notam_type = notam_type
+        if self.notam_type in ('REPLACE', 'CANCEL'):
+            self.ref_notam_id = visited_children[-1]
+
+    def visit_notamn_header(self, *args): return self.visit_notamX_header(*args, notam_type='NEW')
+    def visit_notamr_header(self, *args): return self.visit_notamX_header(*args, notam_type='REPLACE')
+    def visit_notamc_header(self, *args): return self.visit_notamX_header(*args, notam_type='CANCEL')
+
+    visit_icao_id = visit_simple_regex
+    visit_notam_id = visit_simple_regex
+    visit_notam_code = visit_simple_regex
+
+    def visit_q_clause(self, node, visited_children):
+        self.fir = visited_children[2]
+        self.fl_lower = visited_children[12]
+        self.fl_upper = visited_children[14]
+
+    def visit_notam_code(self, *args):
+        self.notam_code = self.visit_simple_regex(*args) # TODO: Parse this into the code's meaning. One day...
+
+    def visit_traffic_type(self, *args):
+        self.traffic_type = self.visit_code_node(*args, meanings={'I' : 'IFR',
+                                                                  'V' : 'VFR',
+                                                                  'K' : 'CHECKLIST'})
+
+    def visit_purpose(self, *args):
+        self.purpose = self.visit_code_node(*args, meanings={'N' : 'IMMEDIATE ATTENTION',
+                                                             'B' : 'OPERATIONAL SIGNIFICANCE',
+                                                             'O' : 'FLIGHT OPERATIONS',
+                                                             'M' : 'MISC',
+                                                             'K' : 'CHECKLIST'})
+
+    def visit_scope(self, *args):
+        self.scope = self.visit_code_node(*args, meanings={'A' : 'AERODROME',
+                                                           'E' : 'EN-ROUTE',
+                                                           'W' : 'NAV WARNING',
+                                                           'K' : 'CHECKLIST'})
+
+    def visit_area_of_effect(self, node, _):
+        self.area = node.match.groupdict() # dictionary containing mappings for 'lat', 'long', and 'radius'
+        self.area['radius'] = int(self.area['radius'])
+
+    def visit_a_clause(self, node, _):
+        def _dfs_icao_id(n):
+            if n.expr_name == "icao_id": return [self.visit_simple_regex(n, [])]
+            return sum([_dfs_icao_id(c) for c in n.children], []) # flatten list-of-lists
+        self.location = _dfs_icao_id(node)
+
+    def visit_b_clause(self, node, visited_children):
+        self.valid_from = visited_children[2]
+
+    def visit_c_clause(self, node, visited_children):
+        if self.has_descendant(node, 'permanent'):
+            dt = datetime.datetime.max
+        else:
+            dt = visited_children[2][0][0]
+            if self.has_descendant(node, 'estimated'):
+                dt = EstimatedDateTime(dt)
+        self.valid_till = dt
+
+    def visit_datetime(self, node, visited_children):
+        dparts = visited_children
+        dparts[0] = 1900 + dparts[0] if dparts[0] > 80 else 2000 + dparts[0] # interpret 2-digit year
+        return datetime.datetime(*dparts, tzinfo=datetime.timezone.utc)
+
+    def visit_intX(self, *args):
+        v = self.visit_simple_regex(*args)
+        return int(v)
+
+    visit_int2 = visit_intX
+    visit_int3 = visit_intX
+
+    def generic_visit(self, _, visited_children):
+        return visited_children
+
+    def visit_root(self, _, __):
+        pass
